ee7e24f33eceb95849577e647901dd9f25cb1f63
steves999 <43765793+steves999@users.noreply.github.com>
2026-06-28 18:52:09 +1200

    Delete Roon Display HDMI/launch_browser.sh

--- Roon Display HDMI/launch_browser.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DISPLAY=:1 xinput set-prop 'QDtech MPI1001' 'Coordinate Transformation Matrix' -1 0 1 0 -1 1 0 0 1 &
-sleep 1
-chromium --kiosk --noerrdialogs --disable-infobars --disable-session-crashed-bubble "$1"
